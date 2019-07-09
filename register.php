975bce277041d89547066396b04ca48491aaeb26
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2019-07-08 19:14:42 -0500

    Add files via upload
    Created multiple functions to sanitize user registration credentials.

--- register.php
+++ register.php
@@ -2,6 +2,26 @@
 
 <?php 
 
+// Sanitize functions for username, password, and generic strings
+
+function sanitizeFormUsername($inputText){
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	return $inputText;
+}
+
+function sanitizeFormString($inputText){
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	$inputText = ucfirst(strtolower($inputText));
+	return $inputText;
+}
+
+function sanitizeFormPassword($inputText){
+	$inputText = strip_tags($inputText);
+	return $inputText;
+}
+
 if(isset($_POST['loginButton'])){
 	//Login button was pressed
 	
@@ -9,8 +29,22 @@ if(isset($_POST['loginButton'])){
 
 if(isset($_POST['registerButton'])){
 	//Login button was pressed
+	$registerUsername = sanitizeFormUsername($_POST['registerUsername']);
+
+	$firstName = sanitizeFormString($_POST['firstName']);
 	
+	$lastName = sanitizeFormString($_POST['lastName']);
+
+	$registerEmail = sanitizeFormString($_POST['registerEmail']);
+
+	$emailConfirmation = sanitizeFormString($_POST['emailConfirmation']);
+
+	$registerPassword = sanitizeFormPassword($_POST['registerPassword']);
+
+	$passwordConfirmation = sanitizeFormPassword($_POST['passwordConfirmation']);
 }
+
+
 ?>
 
 <html>

603d2a3a082791523b3f3183355a2ed0f8272e96
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2019-07-10 14:17:07 -0500

    Add files via upload
    Uploaded the Account class to keep track of user accounts and validation.

---
+++ includes/classes/Account.php
@@ -0,0 +1,32 @@
+<?php 
+    class Account{
+        // Constructor function
+        public function __constructor(){
+            // Validation functions
+        }
+
+        public function register(){
+            
+        }
+
+        private function validateUsername($username){
+    
+        }
+
+        private function validateFirstName($firstName){
+
+        }
+
+        private function validateLastName($lastName){
+
+        }
+
+        private function validateEmails($email, $emailConfirmed){
+
+        }
+
+        private function validatePasswords($password, $passwordConfirmed){
+
+        }
+    }
+?>

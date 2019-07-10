4ee5822fcb7f98aae4aca6220adb74218d9ec4ce
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2019-07-10 14:06:26 -0500

    Add files via upload

--- register.php
+++ register.php
@@ -1,50 +1,8 @@
 <!DOCTYPE html>
 
 <?php 
-
-// Sanitize functions for username, password, and generic strings
-
-function sanitizeFormUsername($inputText){
-	$inputText = strip_tags($inputText);
-	$inputText = str_replace(" ", "", $inputText);
-	return $inputText;
-}
-
-function sanitizeFormString($inputText){
-	$inputText = strip_tags($inputText);
-	$inputText = str_replace(" ", "", $inputText);
-	$inputText = ucfirst(strtolower($inputText));
-	return $inputText;
-}
-
-function sanitizeFormPassword($inputText){
-	$inputText = strip_tags($inputText);
-	return $inputText;
-}
-
-if(isset($_POST['loginButton'])){
-	//Login button was pressed
-	
-}
-
-if(isset($_POST['registerButton'])){
-	//Login button was pressed
-	$registerUsername = sanitizeFormUsername($_POST['registerUsername']);
-
-	$firstName = sanitizeFormString($_POST['firstName']);
-	
-	$lastName = sanitizeFormString($_POST['lastName']);
-
-	$registerEmail = sanitizeFormString($_POST['registerEmail']);
-
-	$emailConfirmation = sanitizeFormString($_POST['emailConfirmation']);
-
-	$registerPassword = sanitizeFormPassword($_POST['registerPassword']);
-
-	$passwordConfirmation = sanitizeFormPassword($_POST['passwordConfirmation']);
-}
-
-
+	include("includes/handlers/register-handler.php");
+	include("includes/handlers/login-handler.php");
 ?>
 
 <html>

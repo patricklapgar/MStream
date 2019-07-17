4a82b7b45c4dca4e56189e8b31349eb6beb87b7e
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2019-07-16 23:00:06 -0500

    Add files via upload
    Included the configuration file to connect the MySQL database.

--- register.php
+++ register.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 
 <?php 
+	include("includes/config.php");
 	include("includes/classes/Account.php");
 	include("includes/classes/Constants.php");
 
-	$account = new Account();
+	$account = new Account($con);
 
 	include("includes/handlers/register-handler.php");
 	include("includes/handlers/login-handler.php");

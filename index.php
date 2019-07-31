97d7b96b875a3bc836fc083856e4ca9a6979d900
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2019-07-30 22:33:21 -0500

    Add files via upload
    Included the shuffle, previous, play, next, and repeat buttons with no functionality yet, only styling. Also built the song progress bar that will change background width and the song progresses.

--- index.php
+++ index.php
@@ -32,8 +32,40 @@
 
 					<div class="buttons">
 						<button class="controlButton shuffle" title="Shuffle">
-							<img src="assets/images/icons/shuffle.png" alt="shuffle icons">
+							<img src="assets/images/icons/shuffle.png" alt="shuffle">
 						</button>
+
+						<button class="controlButton previous" title="Take it back a bit">
+							<img src="assets/images/icons/previous.png" alt="previous">
+						</button>
+						
+						<button class="controlButton play" title="Play dat song">
+							<img src="assets/images/icons/play.png" alt="play">
+						</button>
+
+						<button class="controlButton pause" title="Hold up!" style="display: none;">
+							<img src="assets/images/icons/pause.png" alt="pause">
+						</button>
+						
+						<button class="controlButton next" title="Thank u, next">
+							<img src="assets/images/icons/next.png" alt="next">
+						</button>
+						
+						<button class="controlButton repeat" title="Repeat song">
+							<img src="assets/images/icons/repeat.png" alt="repeat">
+						</button>
+					</div>
+
+					<div class="playbackBar">
+						<span class="progressTime current">0.00</span>
+
+						<div class="progressBar">
+							<div class="progressBarBg">
+								<div class="progress"></div>
+							</div>
+						</div>
+						
+						<span class="progressTime remaining">0.00</span>
 					</div>
 
 				</div>

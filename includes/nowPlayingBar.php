04d2bdd45a8cc0120a752be3ee9dc8dc028ab9ab
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2019-08-05 14:33:02 -0500

    Add files via upload
    Uploaded both files that hold the navigation bar container and the now playing bar container.

---
+++ includes/nowPlayingBar.php
@@ -0,0 +1,80 @@
+<div id="nowPlayingBarContainer">
+    <div id="nowPlayingBar">
+        <div id="nowPlayingLeft">
+                <div class="content">
+                    <span class="albumLink">
+                        <img src="assets/images/artwork/h3ODST.jpg" class="albumArtwork" width="57px">
+                    </span>
+
+                    <div class="trackInfo">
+                        <span class="trackName">
+                            <span>Hello World</span>
+                        </span>
+
+                        <span class="artistName">
+                            <span>Patrick Apgar</span>
+                        </span>
+                    </div>
+
+                </div>
+        </div>
+
+        <div id="nowPlayingCenter">
+            <div class="content playerControls">
+
+                <div class="buttons">
+                    <button class="controlButton shuffle" title="Shuffle">
+                        <img src="assets/images/icons/shuffle.png" alt="shuffle">
+                    </button>
+
+                    <button class="controlButton previous" title="Take it back a bit">
+                        <img src="assets/images/icons/previous.png" alt="previous">
+                    </button>
+                    
+                    <button class="controlButton play" title="Play dat song">
+                        <img src="assets/images/icons/play.png" alt="play">
+                    </button>
+
+                    <button class="controlButton pause" title="Hold up!" style="display: none;">
+                        <img src="assets/images/icons/pause.png" alt="pause">
+                    </button>
+                    
+                    <button class="controlButton next" title="Thank u, next">
+                        <img src="assets/images/icons/next.png" alt="next">
+                    </button>
+                    
+                    <button class="controlButton repeat" title="Repeat song">
+                        <img src="assets/images/icons/repeat.png" alt="repeat">
+                    </button>
+                </div>
+
+                <div class="playbackBar">
+                    <span class="progressTime current">0.00</span>
+
+                    <div class="progressBar">
+                        <div class="progressBarBg">
+                            <div class="progress"></div>
+                        </div>
+                    </div>
+                    
+                    <span class="progressTime remaining">0.00</span>
+                </div>
+
+            </div>
+        </div>
+
+        <div id="nowPlayingRight">
+            <div class="volumeBar">
+                <button class="controlButton volume" title="Volume button">
+                    <img src="assets/images/icons/volume.png" alt="Volume">
+                </button>
+
+                <div class="progressBar">
+                    <div class="progressBarBg">
+                        <div class="progress"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

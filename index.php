4201997921ec2a7f56ea539624aa0c5d970ecae6
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2019-08-09 15:15:38 -0500

    Add files via upload
    Retrieved album artwork covers/titles from the db and used a while loop to display each album cover.

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php include("includes/header.php"); ?>
 					
-<h1>You Might Also Like</h1>
+<h1 class="pageHeadingBig">You Might Also Like</h1>
+
+<!-- 
+    In later versions, create functionality where user hovers 
+    over a tab in the navigation bar and sees a preview of the following page.
+
+    Also, see about changing black background to biege and using the same hover functionality
+    for the music/albums.
+-->
+
+<div class="gridViewContainer">
+
+    <?php 
+        $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+
+        while($row = mysqli_fetch_array($albumQuery)) {
+            // The . syntax joins two or more strings together
+
+            echo "<div class='gridViewItem'>
+
+                <img src='" . $row['artworkPath'] . "'>
+
+                <div class='gridViewInfo'>"
+                    . $row['title'] .
+                "</div>
+
+            </div>";
+        }
+    ?>
+
+</div>
 
 <?php include("includes/footer.php"); ?>

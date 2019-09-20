7bc7ac574d6712cf20d709b5cb50625325114cf9
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2019-09-20 02:02:01 -0500

    Add files via upload
    Created an audio class which holds the audio document object. Multiple functions have also been introduced and utilized to change the volume and update the time at which the song progresses and etc.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,53 @@
 var currentPlaylist = new Array();
 var audioElement;
+var mouseDown = false;
+
+function formatTime(seconds) {
+    var time = Math.round(seconds);
+    var minutes = Math.floor(time / 60); // Rounds down
+    var seconds = time - minutes * 60;
+
+    var extraZero = (seconds < 10) ? "0" : "";
+
+    return minutes + ":" + extraZero + seconds;
+}
+
+function updateTimeProgressBar(audio){
+    $(".progressTime.current").text(formatTime(audio.currentTime));
+    $(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));    
+
+    var progress = audio.currentTime / audio.duration * 100;
+    $(".playbackBar .progress").css("width", progress + "%");
+}
+
+function updateVolumeProgressBar(audio){ 
+    var volume = audio.volume * 100;
+    $(".volumeBar .progress").css("width", volume + "%");
+}
 
 // Audio class
 function Audio() {
 
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
+
+    // Event listeners
+    this.audio.addEventListener("canplay", function() {
+        // 'this' refers to the object that the event was called on
+        // In this case, that object is the 'audio' object
+        var duration = formatTime(this.duration);
+        $(".progressTime.remaining").text(duration);
+    });
+
+    this.audio.addEventListener("timeupdate", function() {
+        if(this.duration ) {
+            updateTimeProgressBar(this);
+        }
+    });
+
+    this.audio.addEventListener("volumechange", function(){
+        updateVolumeProgressBar(this);
+    });
 
     // This function will be called everytime a new song is selected
     this.setTrack = function(track) {
@@ -19,6 +61,10 @@ function Audio() {
 
     this.pause = function() {
         this.audio.pause();
+    }
+
+    this.setTime = function(seconds) {
+        this.audio.currentTime = seconds;
     }
 
 }

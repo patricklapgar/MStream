5a7dd4977d51645008cd426a8efa98b1543c2b37
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2019-10-23 14:21:45 -0500

    Add files via upload
    Created a temporary playlist array to hold a list of songs to be shuffled or unshuffled upon user's request.

--- album.php
+++ album.php
@@ -40,7 +40,7 @@
 
                 echo "<li class='tracklistRow'> 
                         <div class='trackCount'>
-                            <img class='play' src='assets/images/icons/play-white.png'>
+                            <img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(\"" . $albumSong->getId() . "\", tempPlaylist, true)'>
                             <span class='trackNumber'>$i</span>
                         </div>
 
@@ -64,6 +64,14 @@
             }
             
         ?>
+
+            <script>
+                var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+                tempPlaylist = JSON.parse(tempSongIds);
+                
+                
+            </script>
+
     </ul>
 </div>
 

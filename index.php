eb23e97f78f7e80f14c4722f27c6a51ade911a2b
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2019-12-24 01:34:36 -0600

    Add files via upload
    Replaced header and footer files with an 'includedFiles' file which keeps all header/footer features if a page was loaded via AJAX or not. Additionally, I replaced each anchor tag with a span element to create a more dynamic link.

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php include("includes/header.php"); ?>
+<?php 
+    include("includes/includedFiles.php");
+?>
 					
 <h1 class="pageHeadingBig">You Might Also Like</h1>
 
@@ -19,17 +21,16 @@
             // The . syntax joins two or more strings together
 
             echo "<div class='gridViewItem'>
-                    <a href='album.php?id=" . $row['id'] ."'>
+                    <span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'>
                         <img src='" . $row['artworkPath'] . "'>
 
                         <div class='gridViewInfo'>"
                             . $row['title'] .
                         "</div>
-                    </a>
+                    </span>
             </div>";
         }
     ?>
 
 </div>
 
-<?php include("includes/footer.php"); ?>

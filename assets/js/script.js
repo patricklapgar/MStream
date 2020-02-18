fbb9057c593c57cfd4d0085ffad13ad8fd0d6455
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2020-02-18 01:33:21 -0600

    Add files via upload
    Wrote multiple functions to update the user's email and password. Both functions utilize an AJAX call to a separate PHP file to insert query into MySQL DB. There is also a new logout function that ends the user's session and redirects back to register.php. Additionally, there are three other functions, one that creates a playlist, another that deletes a playlist and one more that removes a song from a playlist. All three use an AJAX call to either insert or delete the corresponding data in the database.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,80 @@ var currentIndex = 0;
 var repeat = false;
 var shuffle = false;
 var userLoggedIn;
+var timer;
+
+// function play() {
+//     audio = new Audio();
+//     audio.play;
+// }
+
+function updateEmail(emailClass) {
+    var emailValue = $("." + emailClass).val();
+
+    $.post("includes/handlers/ajax/updateEmail.php", { email: emailValue, username: userLoggedIn })
+    .done(function(response) {
+        $("." + emailClass).nextAll(".message").text(response);
+    })
+}
+
+function updatePassword(oldPasswordClass, newPasswordClass1, newPasswordClass2) {
+    var oldPassword = $("." + oldPasswordClass).val();
+    var newPassword1 = $("." + newPasswordClass1).val();
+    var newPassword2 = $("." + newPasswordClass2).val();    
+
+    $.post("includes/handlers/ajax/updatePassword.php", 
+    {   oldPassword: oldPassword, 
+        newPassword1: newPassword1, 
+        newPassword2: newPassword2, 
+        username: userLoggedIn 
+    }).done(function(response) {
+        $("." + oldPasswordClass).nextAll(".message").text(response);
+    })
+}
+
+function logout() {
+    $.post("includes/handlers/ajax/logout.php", function() {
+        location.reload();
+    });
+}
+
+$(document).click(function(click) {
+    var target = $(click.target);
+
+    if(!target.hasClass("item") && !target.hasClass("optionsButton")) {
+        hideOptionsMenu();
+    }
+});
+
+$(window).scroll(function() {
+    hideOptionsMenu();
+});
+
+$(document).on("change", "select.playlist", function() {
+    var select = $(this);
+    var playlistId = select.val();
+    var songId = select.prev(".songId").val();
+
+    // AJAX call to add a song to playlists
+    $.post("includes/handlers/ajax/addToPlaylist.php", { playlistId: playlistId, songId: songId })
+    .done(function(error) {
+        if(error != ""){
+            alert(error);
+            return;
+        }
+
+        hideOptionsMenu();
+        select.val("");
+    });
+});
 
 // This function will allow seamless page transition from one page to another
 function openPage(url) {
+    
+    if(timer != null) {
+        clearTimeout(timer);
+    }
+
     if(url.indexOf("?") == -1){
         url = url + "?";
     }
@@ -17,6 +88,80 @@ function openPage(url) {
         $("#mainContent").load(encodedUrl);
         $("#body").scrollTop(0);
         history.pushState(null, null, url);
+}
+
+// Remove song function
+function removeFromPlaylist(button, playlistId) {
+    var songId = $(button).prevAll(".songId").val();
+    $.post("includes/handlers/ajax/removeFromPlaylist.php", { playlistId: playlistId, songId: songId })
+    .done(function(error) {
+        // do something when AJAX returns
+        if(error != ""){
+            alert(error);
+            return;
+        }
+        openPage("playlist.php?id=" + playlistId);
+    });
+}
+
+// TODO: use this function as a template for a 'createSong' function later
+function createPlaylist() {
+    // Prompts the user to enter the playlist name
+    var popup = prompt("Please enter the name of your playlist");
+
+    if(popup != null) {
+        // Initiate AJAX call to create a MySQL query
+        $.post("includes/handlers/ajax/createPlaylist.php", { name: popup, username: userLoggedIn })
+        .done(function(error) {
+            // do something when AJAX returns
+            if(error != ""){
+                alert(error);
+                return;
+            }
+            openPage("yourMusic.php");
+        });
+    }
+}
+
+function deletePlaylist(playlistId) {
+    var prompt = confirm("Are you sure you want to delete this playlist?");
+
+    if(prompt){
+        $.post("includes/handlers/ajax/deletePlaylist.php", { playlistId: playlistId })
+        .done(function(error) {
+            // do something when AJAX returns
+            if(error != ""){
+                alert(error);
+                return;
+            }
+            openPage("yourMusic.php");
+        });
+    }
+}
+
+function hideOptionsMenu() {
+	var menu = $(".optionsMenu");
+	if(menu.css("display") != "none") {
+		menu.css("display", "none");
+	}
+}
+
+// Show song options function
+function showOptionsMenu(button) {
+    // 'prev' method goes up one line of code to the next ancestor, and 'prevAll' goes up multiple lines if it has to.
+    var songId = $(button).prevAll(".songId").val();
+	var menu = $(".optionsMenu");
+    var menuWidth = menu.width();
+    menu.find(".songId").val(songId);
+
+	var scrollTop = $(window).scrollTop(); //Distance from top of window to top of document
+	var elementOffset = $(button).offset().top; //Distance from top of document
+
+	var top = elementOffset - scrollTop;
+	var left = $(button).position().left;
+
+	menu.css({ "top": top + "px", "left": left - menuWidth + "px", "display": "inline" });
+
 }
 
 function formatTime(seconds) {
@@ -40,6 +185,10 @@ function updateTimeProgressBar(audio){
 function updateVolumeProgressBar(audio){ 
     var volume = audio.volume * 100;
     $(".volumeBar .progress").css("width", volume + "%");
+}
+
+function playFirstSong() {
+    setTrack(tempPlaylist[0], tempPlaylist, true);
 }
 
 // Audio class

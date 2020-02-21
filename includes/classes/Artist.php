03415d9168fbb76b3712b1792af0d226b8e9d0b2
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2020-02-21 01:26:12 -0600

    Add new files via upload
    Uploaded a new 'Playlist' and 'User' class.

--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -9,10 +9,28 @@
             $this->id = $id;
         }
         
+        public function getId() {
+            return $this->id;
+        }
+        
         public function getName() {
             $artistQuery = mysqli_query($this->con, "SELECT name FROM artists WHERE id='$this->id'");
             $artist = mysqli_fetch_array($artistQuery);
             return $artist['name'];
+        }
+
+        public function getSongIds(){ 
+            $query = mysqli_query($this->con, "SELECT id FROM songs WHERE artist='$this->id' ORDER BY plays ASC");
+        //    initialize new array here
+            $array = array();
+
+            // Push all values pertaining to the id of the the loop is currently on into the created array above
+            while($row = mysqli_fetch_array($query)){
+                array_push($array, $row['id']);
+            }
+
+            // Return the array created
+            return $array;
         }
     }
 
--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -8,7 +8,9 @@
         public static $lastNameCharacters       = "Your last name must be between 2 and 25 characters.";
         public static $firstNameCharacters      = "Your first name must be between 2 and 25 characters.";
         public static $usernameCharacters       = "Your user name must be between 5 and 25 characters.";
-        public static $usernameTaken            = "This username already exists";
+        public static $usernameTaken            = "This username already exists.";
+        public static $emailTaken               = "This email is already in use.";
+        public static $loginFailed              = "Your username or password was incorrect.";
     }
 
 ?>
